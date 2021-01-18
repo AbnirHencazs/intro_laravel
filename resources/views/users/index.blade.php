a3f53a8ca5c44e9af15af42a696cbc66920596a4
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-18 09:50:06 -0600

    Tabla para mostrar usuarios y boton para borrarlos

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -12,7 +12,35 @@
     <body class="antialiased">
         <div class="container">
             <div class="row">
-                <div class="col"></div>
+                <div class="col-sm8 mx-auto">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Email</th>
+                                <th>Nombre</th>
+                                <th>&nbsp;</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->id }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->name }}</td>
+                                <td>
+                                    <form action="{{ route('users.destroy', $user) }}" method="POST">
+                                        @method('DELETE')
+                                        @csrf
+                                        <input type="submit" value="Eliminar" class="btn btn-sm btn-danger"
+                                                onclick="return confirm('¿Desea eliminar?')">
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach()
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </body>
